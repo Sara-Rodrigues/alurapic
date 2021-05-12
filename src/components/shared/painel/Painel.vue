1ccc8709c0891882773cedc9f4980d22042b9c09
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-05-11 21:33:09 -0300

    Trabalhando com slots

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -1,13 +1,9 @@
 <template>
-
     <div class="painel">
-
       <h2 class="painel-titulo">{{ titulo }}</h2>
-      <div class="painel-conteudo">
-
-      </div>
+      <slot class="painel-conteudo">
+      </slot>
     </div>
-
 </template>
 
 <script>
