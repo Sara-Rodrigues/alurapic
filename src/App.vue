a16b8600c8c559f77811d1458900d0de15d1ee87
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-05-17 20:16:40 -0300

    Animando transições de página

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,11 @@
           </li>
       </ul>
     </nav>
-    <router-view></router-view>
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+    
   </div>
 </template>
 
@@ -29,5 +33,13 @@ export default {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
+  }
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .3s
+  }
+
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
   }
 </style>
