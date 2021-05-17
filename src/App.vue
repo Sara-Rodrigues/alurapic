93a25ec5483a381f5611960bfd6e04618e71176e
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-05-17 19:10:36 -0300

    Que tal mais um component?

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
 
         <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
         </meu-painel>
 
       </li>
@@ -19,11 +19,13 @@
 
 <script>
 import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue'
 
 
 export default {
   components: {
-    'meu-painel': Painel
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
   },
   data() {
     return {
