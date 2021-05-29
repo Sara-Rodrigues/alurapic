82728225e392c33d5dce4c58dae2a660e1658ce2
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-05-29 18:58:28 -0300

    Rotas nomeadas

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,43 @@
 <template>
   <div class="corpo">
-
-    <meu-menu :rotas="routes"/>
+    <meu-menu :rotas="routes" />
 
     <transition name="pagina">
       <router-view></router-view>
     </transition>
-
   </div>
 </template>
 
 <script>
-import { routes }  from './routes';
-import Menu from './components/shared/menu/Menu.vue';
+import { routes } from "./routes";
+import Menu from "./components/shared/menu/Menu.vue";
 
 export default {
   components: {
-    'meu-menu' : Menu
+    "meu-menu": Menu,
   },
   data() {
     return {
-      routes
-    }
-  }
-}
-
+      routes: routes.filter((route) => route.menu),
+    };
+  },
+};
 </script>
 
 <style>
-  .corpo {
-    font-family: Helvetica, sans-serif;
-    margin: 0 auto;
-    width: 96%;
-  }
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
 
-  .pagina-enter-active, .pagina-leave-active {
-    transition: opacity .3s
-  }
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.3s;
+}
 
-  .pagina-enter, .pagina-leave-active {
-    opacity: 0
-  }
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
+}
 </style>
