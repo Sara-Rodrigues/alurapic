d36d5f9436442c16e4867afb719ca6fbb3040184
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-05-30 20:42:42 -0300

    Implementando a logica de alteraçao

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h1 class="centralizado">Cadastro</h1>
+    <h1 v-if="foto._id" class="centralizado">Alteração</h1>
+    <h1 v-else class="centralizado">Inclusão</h1>
     <h2 class="centralizado">{{ foto.titulo }}</h2>
 
     <form @submit.prevent="grava()">
@@ -61,7 +62,10 @@ export default {
   methods: {
     grava() {
       this.service.cadastra(this.foto).then(
-        () => (this.foto = new Foto()),
+        () => {
+          if (this.id) this.$router.push({ name: "home" });
+          this.foto = new Foto();
+        },
         (err) => console.log(err)
       );
     },
@@ -69,7 +73,7 @@ export default {
   created() {
     this.service = new FotoService(this.$resource);
     if (this.id) {
-      this.service.busca(this.id).then(foto => this.foto = foto);
+      this.service.busca(this.id).then((foto) => (this.foto = foto));
     }
   },
 };
